7299a2c3a76d31edbb429fdbcdc8d7ce089f7b02
Lachlan Robertson <lachlan.robertson142@gmail.com>
2017-04-15 10:46:24 +1000

    Spacing and handleClick

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import logo from '../img/logo.svg';
 
 class Thumbnail extends Component {
+  handleClick() {
+    console.log("CLICKED!");
+  }
+
   render() {
     return (
-      <div className="Thumbnail">
+      <div className="Thumbnail" onClick={this.handleClick}>
         <img className="Thumbnail__image" src={logo} alt="" />
         <p className="Thumbnail__name">Name</p>
       </div>
